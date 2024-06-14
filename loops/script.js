7c3216d9a823c71ba803bc60bac076384a81b1d1
Mati Tonello <matias.tonello@gm2dev.com>
2024-06-14 11:56:19 -0300

    feat: more excersises to loops

--- loops/script.js
+++ loops/script.js
@@ -116,3 +116,160 @@ const arrayMultiplos = (a , b) => {
   };
   console.log(arrayMultiplos(2, 10));
 console.log(arrayMultiplos(17, 6));
+
+
+
+function arrayContiene(array, elemento) {
+    // Comprueba si el elemento existe dentro de "array"
+    // Devuelve "true" si está, o "false" si no está
+    // Tu código:
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === elemento) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  function agregarNumeros(numeros) {
+    // "numeros" debe ser un arreglo de enteros (int/integers)
+    // Suma todos los enteros y devuelve el valor
+    // Tu código:
+    let suma = 0;
+  
+    for (let i = 0; i < numeros.length; i++) {
+      suma += numeros[i];
+    }
+    return suma;
+  }
+  
+  function promedioResultadosTest(resultadosTest) {
+    // "resultadosTest" debe ser una matriz de enteros (int/integers)
+    // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
+    // Tu código:
+    let suma = 0;
+  
+    for (let i = 0; i < resultadosTest.length; i++) {
+      suma += resultadosTest[i];
+    }
+    return suma / resultadosTest.length;
+  }
+  
+  function numeroMasGrande(numeros) {
+    // "numeros" debe ser una matriz de enteros (int/integers)
+    // Devuelve el número más grande
+    // Tu código:
+    let numero_mas_grande = 0;
+  
+    for (let i = 0; i < numeros.length; i++) {
+      if (numeros[i] > numero_mas_grande) {
+        numero_mas_grande = numeros[i];
+      }
+    }
+    return numero_mas_grande;
+  }
+
+  
+  function cuentoElementos(arreglo) {
+    //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
+    //Escribe tu código aquí
+    let cantidad_elementos = 0;
+  
+    for (let i = 0; i < arreglo.length; i++) {
+      if (arreglo[i] > 18) {
+        cantidad_elementos++;
+      }
+    }
+    return cantidad_elementos;
+  }
+  
+  
+  function todosIguales(arreglo) {
+    //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
+    //retornar true, caso contrario retornar false.
+    //Escribe tu código aquí
+    for (let i = 1; i < arreglo.length; i++) {
+      if (arreglo[i] != arreglo[i - 1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  function mesesDelAño(array) {
+    //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de
+    // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
+    //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
+    // Tu código:
+    let array_meses = [];
+  
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == "Enero" || array[i] == "Marzo" || array[i] == "Noviembre") {
+        array_meses.push(array[i]);
+      }
+    }
+    if (array_meses.length > 2) {
+      return array_meses;
+    } else {
+      return "No se encontraron los meses pedidos";
+    }
+  }
+  
+  function mayorACien(array) {
+    //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
+    //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
+    // Tu código:
+    nuevo_array = [];
+  
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > 100) {
+        nuevo_array.push(array[i]);
+      }
+    }
+    return nuevo_array;
+  }
+  
+  function breakStatement(numero) {
+    //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
+    //Guardar cada nuevo valor en un array.
+    //Devolver el array
+    //Si en algún momento el valor de la suma y la cantidad de iteraciones coinciden, debe interrumpirse la ejecución y
+    //devolver: "Se interrumpió la ejecución"
+    //Pista: usá el statement 'break'
+    // Tu código:
+    let iteraciones = 0;
+    let array = [];
+    valor_de_la_suma = 0;
+  
+    for (let i = 0; i < 10; i++) {
+      array.push(parseInt(numero) + 2);
+      numero = array[i];
+      valor_de_la_suma = valor_de_la_suma + array[i];
+      iteraciones++;
+  
+      if (iteraciones == valor_de_la_suma) {
+        return "Se interrumpió la ejecución";
+      }
+    }
+    return array;
+  }
+  
+  function continueStatement(numero) {
+    //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
+    //Guardar cada nuevo valor en un array.
+    //Devolver el array
+    //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
+    //Pista: usá el statement 'continue'
+    // Tu código:
+    let array = [];
+  
+    for (let i = 0; i < 10; i++) {
+      if (i != 5) {
+        array.push(numero + 2);
+        numero = numero + 2;
+      } else {
+        continue;
+      }
+    }
+    return array;
+  }
